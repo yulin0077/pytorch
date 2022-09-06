d1c1f929fc1986fb2a86ac5abae40ded19f27b72
Peter Bell <peterbell10@live.co.uk>
2022-09-06 15:42:42 +0100

    Fix linalg.norm sample inputs function and related failures (#84452)
    Due to an indentation error, the return statement happens after just 1
    loop of `for test_size in test_sizes` so only one shape was ever
    tested.
    
    This also revealed several cases where the provided shapes don't work
    so I've disabled the generation of those sample inputs.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84452
    Approved by: https://github.com/Lezcano, https://github.com/zou3519

--- torch/testing/_internal/opinfo/definitions/linalg.py
+++ torch/testing/_internal/opinfo/definitions/linalg.py
@@ -533,8 +533,11 @@ def sample_inputs_linalg_norm(
         is_vector_norm = len(test_size) == 1
         is_matrix_norm = len(test_size) == 2
 
+        # IndexError: amax(): Expected reduction dim 0 to have non-zero size.
+        is_valid_for_p2 = is_vector_norm or (test_size[-1] != 0 and test_size[-2] != 0)
+
         for keepdim in [False, True]:
-            if not variant == "subgradient_at_zero":
+            if variant != "subgradient_at_zero" and is_valid_for_p2:
                 inputs.append(
                     SampleInput(
                         make_tensor(
@@ -555,6 +558,28 @@ def sample_inputs_linalg_norm(
             ords = vector_ords if is_vector_norm else matrix_ords
 
             for ord in ords:
+                if is_vector_norm and test_size[-1] == 0:
+                    if ord == np.inf or (ord is not None and ord < 0):
+                        # RuntimeError: linalg.vector_norm cannot compute the
+                        # {ord} norm on an empty tensor because the operation
+                        # does not have an identity
+                        continue
+                elif is_matrix_norm:
+                    dims_to_check = {
+                        None: (0,),
+                        np.inf: (0,),
+                        2: (0, 1),
+                        1: (1,),
+                        -1: (1,),
+                        -2: (0, 1),
+                        -np.inf: (0,),
+                    }.get(ord, ())
+
+                    if any(test_size[d] == 0 for d in dims_to_check):
+                        # IndexError: amax(): Expected reduction dim {dim} to
+                        # have non-zero size.
+                        continue
+
                 if variant == "subgradient_at_zero":
                     inputs.append(
                         SampleInput(
@@ -599,7 +624,7 @@ def sample_inputs_linalg_norm(
                             )
                         )
 
-        return inputs
+    return inputs
 
 
 def sample_inputs_linalg_vecdot(op_info, device, dtype, requires_grad, **kwargs):
@@ -1734,6 +1759,27 @@ op_db: List[OpInfo] = [
         supports_forward_ad=True,
         check_batched_forward_grad=False,
         supports_fwgrad_bwgrad=True,
+        skips=(
+            DecorateInfo(unittest.expectedFailure, "TestGradients", "test_fn_gradgrad"),
+            # RuntimeError: linalg.matrix_norm: Low precision dtypes not supported.
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestSchemaCheckModeOpInfo",
+                "test_schema_correctness",
+                dtypes=(torch.float16, torch.bfloat16),
+            ),
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestDecomp",
+                "test_comprehensive",
+                dtypes=(torch.float16, torch.bfloat16),
+            ),
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestMeta",
+                dtypes=(torch.float16, torch.bfloat16),
+            ),
+        ),
     ),
     OpInfo(
         "linalg.norm",
@@ -1756,6 +1802,28 @@ op_db: List[OpInfo] = [
             DecorateInfo(unittest.expectedFailure, "TestGradients", "test_fn_gradgrad"),
             DecorateInfo(
                 unittest.expectedFailure, "TestGradients", "test_fn_fwgrad_bwgrad"
+            ),
+            DecorateInfo(
+                unittest.expectedFailure, "TestGradients", "test_forward_mode_AD"
+            ),
+            DecorateInfo(unittest.expectedFailure, "TestGradients", "test_fn_grad"),
+            # RuntimeError: linalg.matrix_norm: Low precision dtypes not supported.
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestSchemaCheckModeOpInfo",
+                "test_schema_correctness",
+                dtypes=(torch.float16, torch.bfloat16),
+            ),
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestDecomp",
+                "test_comprehensive",
+                dtypes=(torch.float16, torch.bfloat16),
+            ),
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestMeta",
+                dtypes=(torch.float16, torch.bfloat16),
             ),
         ),
     ),
@@ -2312,6 +2380,13 @@ python_ref_db: List[OpInfo] = [
         # https://github.com/pytorch/pytorch/issues/77216
         validate_view_consistency=False,
         op_db=op_db,
+        skips=(
+            # RuntimeError: linalg.matrix_norm: Half precision dtypes not supported. Got torch.bfloat16
+            DecorateInfo(
+                unittest.expectedFailure,
+                dtypes=(torch.float16, torch.bfloat16),
+            ),
+        ),
     ),
     PythonRefInfo(
         "_refs.linalg.svd",
